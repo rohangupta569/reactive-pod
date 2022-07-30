9c7cf1fedf22d47a8d17c5c2b41f43bb1537907c
Rohan Gupta <rohangupta569@gmail.com>
2022-07-30 18:12:18 +0530

    app styled and added backgrounds

--- src/Screens/AllSongs.js
+++ src/Screens/AllSongs.js
@@ -53,7 +53,14 @@ class AllSongs extends React.Component
                 playPauseButtonClicked={this.props.playPauseButtonClicked}
             />;
         }
-        return (this.state.loading ? <h1>Loading...</h1> :
+        return (this.state.loading ? 
+
+            <div className='loading-screen'>
+                <h1>Loading...</h1>
+                <div class="loader"></div>
+                <div>Please make sure that you are connected to the internet. Songs are fetched from the firebase storage.</div>
+            </div>
+            :
             <div className="all-songs">
                 <h1 className="all-songs-heading">
                     All Songs
